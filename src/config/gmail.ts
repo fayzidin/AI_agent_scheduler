07a6753ef02eb498694000e957562ad832f851b0
Fayziddin Sayliyev <60594333+fayzidin@users.noreply.github.com>
2025-06-28 16:25:16 +0500

    Updated gmail.ts

--- src/config/gmail.ts
+++ src/config/gmail.ts
@@ -22,7 +22,11 @@ export const getGmailConfig = (): GmailConfig => {
     // Updated scopes for better compatibility - removed problematic scopes
     scopes: [
       'https://www.googleapis.com/auth/gmail.readonly', // Read Gmail emails
-      'https://www.googleapis.com/auth/userinfo.email' // Get user email only
+      'https://www.googleapis.com/auth/userinfo.email', // Get user email only
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/calendar.events',
+      'openid'
     ]
   };
 };
